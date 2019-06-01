6b5bf7fb15ab95937688c5ed511cdc40b4c36610
zangfenziang <zangfenziang@126.com>
2019-06-01 20:49:00 +0800

    Add Account

--- src/main/java/hello/control/StockController.java
+++ src/main/java/hello/control/StockController.java
@@ -1,6 +1,7 @@
 package hello.control;
 
 import hello.model.*;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.util.DigestUtils;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
@@ -20,8 +22,8 @@ public class StockController {
     private final static int maxAge = 60 * 60 * 24 * 90;
     private final static String BankerCookieName = "BankerStockCookie";
     private final static String BankerCookieIdName = "BankerStockCookieId";
-    private final static String UserCookieName = "UserStockCookie";
-    private final static String UserCookieIdName = "UserStockCookieId";
+    private final static String AccountCookieName = "UserStockCookie";
+    private final static String AccountCookieIdName = "UserStockCookieId";
     private final static String LegalUserType = "Legal";
     private final static String PersonalUserType = "Personal";
     private final static String LogoutType = "Logout";
@@ -42,7 +44,7 @@ public class StockController {
     private String getBankStockCookie(String id, String password){
         return DigestUtils.md5DigestAsHex((id + DigestUtils.md5DigestAsHex(password.getBytes())).getBytes());
     }
-    private String getUserStockCookie(String id, String password){
+    private String getAccountStockCookie(String id, String password){
         return DigestUtils.md5DigestAsHex((id + DigestUtils.md5DigestAsHex(password.getBytes())).getBytes());
     }
 
@@ -65,16 +67,16 @@ public class StockController {
     }
 
     @RequestMapping("/banker_login")
-    public SimpleStatus bankLogin(@RequestParam String user_id
-            , @RequestParam String user_password
+    public SimpleStatus bankLogin(@RequestParam String id
+            , @RequestParam String password
             , @RequestParam(defaultValue = "0") int remember_status
             , HttpServletResponse response) {
-        user_password = DigestUtils.md5DigestAsHex(user_password.getBytes());
-        List<CapitalAccountBanker> list = capitalAccountBankerRepository.getBankerLogin(user_id, user_password);
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+        List<CapitalAccountBanker> list = capitalAccountBankerRepository.getBankerLogin(id, password);
         if (list.size() > 0){
-            String stockCookie = getBankStockCookie(user_id, user_password);
+            String stockCookie = getBankStockCookie(id, password);
             Cookie cookie = new Cookie(BankerCookieName, stockCookie);
-            Cookie cookieId = new Cookie(BankerCookieIdName, user_id);
+            Cookie cookieId = new Cookie(BankerCookieIdName, id);
             return setCookie(response, remember_status, cookie, cookieId);
         }
         else{
@@ -93,4 +95,76 @@ public class StockController {
         return new SimpleStatus(3, "please login again");
     }
 
+    @RequestMapping("/account_login")
+    public SimpleStatus accountLogin(@RequestParam String user_id
+            , @RequestParam String login_pwd
+            , @RequestParam(defaultValue = "0") int remember_status
+            , HttpServletResponse response){
+        login_pwd = DigestUtils.md5DigestAsHex(login_pwd.getBytes());
+        List<CapitalAccount> list = capitalAccountRepository.getAccountLogin(user_id, login_pwd);
+        if (list.size() == 0){
+            return new SimpleStatus(1, "login failed please try again");
+        }
+        String accountCookie = getAccountStockCookie(user_id, login_pwd);
+        Cookie cookie = new Cookie(AccountCookieName, accountCookie);
+        Cookie cookieId = new Cookie(AccountCookieIdName, user_id);
+        return setCookie(response, remember_status, cookie, cookieId);
+    }
+
+    @RequestMapping("/account_login_status")
+    public SimpleStatus getAccountLoginStatus(@CookieValue(value = AccountCookieIdName, defaultValue = "") String id
+            , @CookieValue(value = AccountCookieName, defaultValue = "") String cookie){
+        List<CapitalAccount> list = capitalAccountRepository.getAccount(id);
+        for (CapitalAccount account: list) {
+            if (cookie.equals(getBankStockCookie(account.getUser_id(), account.getLogin_pwd()))) {
+                return new SimpleStatus(0, id);
+            }
+        }
+        return new SimpleStatus(3, "please login again");
+    }
+
+    private CapitalAccount getAccount(String id, String cookie) throws Exception{
+        List<CapitalAccount> list = capitalAccountRepository.getAccount(id);
+        if (list.size() == 0){
+            throw new Exception("please login again");
+        }
+        CapitalAccount account = list.get(0);
+        if (!cookie.equals(getBankStockCookie(account.getUser_id(), account.getLogin_pwd()))) {
+            throw new Exception("please login again");
+        }
+        return account;
+    }
+
+    @RequestMapping("/account_fund")
+    public SimpleStatus getAccountFund(@CookieValue(value = AccountCookieIdName, defaultValue = "") String id
+            , @CookieValue(value = AccountCookieName, defaultValue = "") String cookie){
+        try{
+            CapitalAccount account = getAccount(id, cookie);
+            return new SimpleStatus(0, account.getFund().toString());
+        }
+        catch (Exception e){
+            return new SimpleStatus(3, e.getMessage());
+        }
+    }
+
+    @Transactional
+    @RequestMapping("/account_fund_change")
+    public SimpleStatus setAccountFund(@CookieValue(value = AccountCookieIdName, defaultValue = "") String id
+            , @CookieValue(value = AccountCookieName, defaultValue = "") String cookie
+            , @RequestParam double number){
+        try{
+            CapitalAccount account = getAccount(id, cookie);
+            BigDecimal fund = account.getFund();
+            if (fund.doubleValue() + number < 0){
+                return new SimpleStatus(4, "fund less than zero");
+            }
+            BigDecimal delta = new BigDecimal(number);
+            BigDecimal result = fund.add(delta);
+            account.setFund(result);
+            return new SimpleStatus(0, "success");
+        }
+        catch (Exception e){
+            return new SimpleStatus(3, e.getMessage());
+        }
+    }
 }
--- src/main/java/hello/model/CapitalAccountRepository.java
+++ src/main/java/hello/model/CapitalAccountRepository.java
@@ -1,6 +1,13 @@
 package hello.model;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CapitalAccountRepository extends CrudRepository<CapitalAccount, Integer> {
+    @Query(value = "select p from CapitalAccount p where p.user_id = ?1 and p.login_pwd = ?2")
+    List<CapitalAccount> getAccountLogin(String user_id, String login_pwd);
+    @Query(value = "select p from CapitalAccount p where p.user_id = ?1")
+    List<CapitalAccount> getAccount(String user_id);
 }
